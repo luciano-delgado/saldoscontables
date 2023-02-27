5bbe9a0431ac428edba75bf8d368e348e8db7686
botsprocesos <109240003+botsprocesos@users.noreply.github.com>
2023-02-27 13:29:22 -0300

    27/2/23: Limpieza cred

--- conn.py
+++ conn.py
@@ -2,9 +2,9 @@ from hdbcli import dbapi
 
 def conexion_bd(amb):
     if amb == 'QAS':
-        conn=dbapi.connect(address="172.31.0.138",port="30115",user="OYP",password="5tAgt7S8k7XvDx",sslValidateCertificate=False)
+        conn=dbapi.connect(address="###",port="###",user="###",password="###",sslValidateCertificate=False)
     elif amb == 'PRD':
-        conn = dbapi.connect(address="172.31.0.130",port="30115",user="OYP",password="A112ShhtPLZYVv",sslValidateCertificate=False)  # PRD
+        conn = dbapi.connect(address="###",port="###",user="###",password="###",sslValidateCertificate=False)  # PRD
     
     return conn
 
@@ -13,9 +13,9 @@ class Conexion:
 
     def connection_bd(self, amb):       #SELF: para que el método sepa que objeto/instancia lo llamo (si no paso el self sería como que le estoy pasando 2 parametros a la funcion (por las 2 instancias)))
         if amb == 'QAS':
-            conn=dbapi.connect(address="172.31.0.138",port="30115",user="OYP",password="5tAgt7S8k7XvDx",sslValidateCertificate=False)
+            conn=dbapi.connect(address="###",port="###",user="###",password="###",sslValidateCertificate=False)
         elif amb == 'PRD':
-            conn = dbapi.connect(address="172.31.0.130",port="30115",user="OYP",password="A112ShhtPLZYVv",sslValidateCertificate=False)  # PRD
+            conn = dbapi.connect(address="###",port="###",user="###",password="###",sslValidateCertificate=False)  # PRD
         
         return conn
 
